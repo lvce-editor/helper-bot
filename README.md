d3bcafac141e469f6fb32460a1fc22ef6c83f14a
Le Vivilet <levivlet@pm.me>
2023-12-28 12:17:39 +0100

    feature: add probot app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# helper-bot
+# Lvce Editor Helper Bot
+
+GitHub App to automaticlly update lvce editor extensions to latest versions.
---
+++ src/index.js
@@ -0,0 +1,100 @@
+/**
+ *
+ * @param {any[]} value
+ * @param {string} version
+ * @returns
+ */
+const getNewValue = (value, version) => {
+  return value.map((item) => {
+    if (item.name === 'b') {
+      return {
+        ...item,
+        version,
+      }
+    }
+    return item
+  })
+}
+
+/**
+ * @param {import('probot').Context<"release">} context
+ */
+const handleReleaseReleased = async (context) => {
+  const { payload, octokit } = context
+  const tagName = payload.release.tag_name
+  const owner = payload.repository.owner.login
+  const baseBranch = 'main'
+  const repo = 'test-repo-a'
+  const filesPath = 'files.json'
+  console.log(tagName)
+  console.log('release was released' + payload.repository.name)
+
+  const newBranch = `update-version/${tagName}`
+
+  const filesJson = await context.octokit.rest.repos.getContent({
+    owner,
+    repo,
+    path: filesPath,
+  })
+
+  if (!('content' in filesJson.data)) {
+    return
+  }
+  const filesJsonBase64 = filesJson.data.content
+  const filesJsonDecoded = Buffer.from(filesJsonBase64, 'base64').toString()
+  const filesJsonValue = JSON.parse(filesJsonDecoded)
+  const filesJsonValueNew = getNewValue(filesJsonValue, tagName)
+  const filesJsonStringNew = JSON.stringify(filesJsonValueNew, null, 2) + '\n'
+  if (filesJsonDecoded === filesJsonStringNew) {
+    return
+  }
+  const filesJsonBase64New = Buffer.from(filesJsonStringNew).toString('base64')
+
+  const mainBranchRef = await octokit.rest.git.getRef({
+    owner,
+    repo,
+    ref: `heads/${baseBranch}`,
+  })
+
+  await octokit.rest.git.createRef({
+    owner,
+    repo,
+    ref: `refs/heads/${newBranch}`,
+    sha: mainBranchRef.data.object.sha,
+  })
+  console.log('created branch')
+
+  await context.octokit.repos.createOrUpdateFileContents({
+    owner,
+    repo,
+    path: filesPath,
+    message: `update to version ${tagName}`,
+    content: filesJsonBase64New,
+    branch: newBranch,
+    sha: filesJson.data.sha,
+  })
+  const pullRequestData = await octokit.rest.pulls.create({
+    owner,
+    repo,
+    head: newBranch,
+    base: baseBranch,
+    title: `update to version ${tagName}`,
+  })
+  await octokit.graphql(
+    `mutation MyMutation {
+  enablePullRequestAutoMerge(input: { pullRequestId: "${pullRequestData.data.node_id}", mergeMethod: SQUASH }) {
+    clientMutationId
+  }
+}
+`,
+    {},
+  )
+}
+
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Probot} app
+ */
+module.exports = (app) => {
+  app.on('release.released', handleReleaseReleased)
+}
