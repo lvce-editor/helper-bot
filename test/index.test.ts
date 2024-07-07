67bfd81e0b302930708a2794121f726724198959
Le Vivilet <levivlet@pm.me>
2024-07-07 23:25:16 +0200

    feature: add script to automatically update renderer process version

--- test/index.test.ts
+++ test/index.test.ts
@@ -149,3 +149,89 @@ test("doesn't create a pull request when the new file content would be the same"
   })
   expect(mock.pendingMocks()).toEqual([])
 })
+
+test.only('creates a pull request to update renderer process version when a release is created', async () => {
+  const mock = nock('https://api.github.com')
+    .get('/repos/lvce-editor/lvce-editor/git/ref/heads%2Fmain')
+    .reply(200, {
+      object: {
+        sha: 'main-sha',
+      },
+    })
+    .post('/repos/lvce-editor/lvce-editor/git/refs', (body) => {
+      expect(body).toEqual({
+        ref: 'refs/heads/update-version/language-basics-css-v2.4.0',
+        sha: 'main-sha',
+      })
+      return true
+    })
+    .reply(200, {})
+    .get(
+      '/repos/lvce-editor/lvce-editor/contents/packages%2Fbuild%2Fsrc%2Fparts%2FDownloadBuiltinExtensions%2FbuiltinExtensions.json',
+    )
+    .reply(200, {
+      content: Buffer.from(
+        JSON.stringify([
+          {
+            name: 'builtin.language-basics-css',
+            version: '2.3.0',
+          },
+        ]),
+      ),
+    })
+    .put(
+      '/repos/lvce-editor/lvce-editor/contents/packages%2Fbuild%2Fsrc%2Fparts%2FDownloadBuiltinExtensions%2FbuiltinExtensions.json',
+      (body) => {
+        expect(body).toEqual({
+          branch: 'update-version/language-basics-css-v2.4.0',
+          content:
+            'WwogIHsKICAgICJuYW1lIjogImJ1aWx0aW4ubGFuZ3VhZ2UtYmFzaWNzLWNzcyIsCiAgICAidmVyc2lvbiI6ICIyLjQuMCIKICB9Cl0K',
+          message: 'feature: update language-basics-css to version v2.4.0',
+        })
+        return true
+      },
+    )
+    .reply(200)
+    .post(`/repos/lvce-editor/lvce-editor/pulls`, (body) => {
+      expect(body).toEqual({
+        base: 'main',
+        head: 'update-version/language-basics-css-v2.4.0',
+        title: 'feature: update language-basics-css to version v2.4.0',
+      })
+      return true
+    })
+    .reply(200, {
+      node_id: 'test-node-id',
+    })
+    .post('/graphql', (body) => {
+      expect(body).toEqual({
+        query: `mutation MyMutation {
+  enablePullRequestAutoMerge(input: { pullRequestId: \"test-node-id\", mergeMethod: SQUASH }) {
+    clientMutationId
+  }
+}
+`,
+      })
+      return true
+    })
+    .reply(200)
+
+  // Receive a webhook event
+  await probot.receive({
+    name: 'release',
+    payload: {
+      action: 'released',
+      release: {
+        tag_name: 'v2.4.0',
+      },
+      repository: {
+        name: 'renderer-process',
+        // @ts-ignore
+        owner: {
+          login: 'lvce-editor',
+        },
+      },
+    },
+  })
+  expect(mock.pendingMocks()).toEqual([])
+})
