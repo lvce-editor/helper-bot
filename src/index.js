45d1db438555bb51d8359376e3993c4e3962588f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-07 23:58:09 +0200

    feature: add script to automatically update renderer process version (#28)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const { mkdir, writeFile, readFile } = require('node:fs/promises')
+const { tmpdir } = require('node:os')
+const { join } = require('node:path')
+
 /**
  *
  * @param {any[]} value
@@ -24,6 +28,9 @@ const getNewValue = (value, repoName, version) => {
  * @returns {string}
  */
 const getCommitMessage = (releasedRepo, tagName) => {
+  if (releasedRepo === 'renderer-process') {
+    return `feature: update ${releasedRepo} to version ${tagName}`
+  }
   if (
     releasedRepo.startsWith('language-basics') ||
     releasedRepo.startsWith('language-features')
@@ -34,15 +41,34 @@ const getCommitMessage = (releasedRepo, tagName) => {
 }
 
 /**
+ * @param {import('probot').Context<"release">['octokit']} octokit
+ * @param {any} pullRequestData
+ */
+const enableAutoSquash = async (octokit, pullRequestData) => {
+  await octokit.graphql(
+    `mutation MyMutation {
+  enablePullRequestAutoMerge(input: { pullRequestId: "${pullRequestData.data.node_id}", mergeMethod: SQUASH }) {
+    clientMutationId
+  }
+}
+`,
+    {},
+  )
+}
+
+/**
  * @param {import('probot').Context<"release">} context
  */
-const handleReleaseReleased = async (context) => {
+const updateBuiltinExtensions = async (context) => {
   const { payload, octokit } = context
   const tagName = payload.release.tag_name
   const owner = payload.repository.owner.login
   const baseBranch = 'main'
   const repo = 'lvce-editor'
   const releasedRepo = payload.repository.name
+  if (releasedRepo === 'renderer-process') {
+    return
+  }
   const filesPath =
     'packages/build/src/parts/DownloadBuiltinExtensions/builtinExtensions.json'
   const version = tagName.replace('v', '')
@@ -101,15 +127,146 @@ const handleReleaseReleased = async (context) => {
     base: baseBranch,
     title: `feature: update ${releasedRepo} to version ${tagName}`,
   })
-  await octokit.graphql(
-    `mutation MyMutation {
-  enablePullRequestAutoMerge(input: { pullRequestId: "${pullRequestData.data.node_id}", mergeMethod: SQUASH }) {
-    clientMutationId
-  }
+  await enableAutoSquash(octokit, pullRequestData)
 }
-`,
-    {},
+
+/**
+ *
+ * @param {any} oldPackageJson
+ * @param {string} newVersion
+ * @returns
+ */
+const getNewRendererWorkerPackageJson = (oldPackageJson, newVersion) => {
+  oldPackageJson.dependencies['@lvce-editor/renderer-process'] =
+    `^${newVersion}`
+  return oldPackageJson
+}
+
+/**
+ * @param {import('probot').Context<"release">} context
+ */
+const updateRendererProcessVersion = async (context) => {
+  const { payload, octokit } = context
+  const tagName = payload.release.tag_name
+  const owner = payload.repository.owner.login
+  const releasedRepo = payload.repository.name
+  if (releasedRepo !== 'renderer-process') {
+    return
+  }
+  const packageJsonPath = 'packages/renderer-worker/package.json'
+  const packageLockJsonPath = 'packages/renderer-worker/package-lock.json'
+  const version = tagName.replace('v', '')
+
+  const newBranch = `update-version/${releasedRepo}-${tagName}`
+  const baseBranch = 'main'
+  const repo = 'lvce-editor'
+
+  const packageJsonRef = await context.octokit.rest.repos.getContent({
+    owner,
+    repo,
+    path: packageJsonPath,
+  })
+  const packageLockJsonRef = await context.octokit.rest.repos.getContent({
+    owner,
+    repo,
+    path: packageLockJsonPath,
+  })
+
+  if (
+    !('content' in packageJsonRef.data) ||
+    !('content' in packageLockJsonRef.data)
+  ) {
+    console.log('no content in files')
+    return
+  }
+
+  const filesJsonBase64 = packageJsonRef.data.content
+  const filesJsonDecoded = Buffer.from(filesJsonBase64, 'base64').toString()
+  const filesJsonValue = JSON.parse(filesJsonDecoded)
+  console.log({ filesJsonValue })
+  const oldVersion =
+    filesJsonValue.dependencies[`@lvce-editor/${releasedRepo}`].slice(1)
+
+  console.log({ oldVersion })
+  if (oldVersion === version) {
+    console.info('same version')
+    return
+  }
+  const filesJsonValueNew = getNewRendererWorkerPackageJson(
+    filesJsonValue,
+    version,
   )
+  const packageJsonStringNew = JSON.stringify(filesJsonValueNew, null, 2) + '\n'
+  const tmpFolder = join(tmpdir(), 'renderer-process-release')
+  console.log({ tmpFolder })
+  await mkdir(tmpFolder, { recursive: true })
+  await writeFile(join(tmpFolder, 'package.json'), packageJsonStringNew)
+  const { execa } = await import('execa')
+  await execa(`npm`, ['install'], {
+    cwd: tmpFolder,
+  })
+  const newPackageLockJson = await readFile(
+    join(tmpFolder, 'package-lock.json'),
+    'utf8',
+  )
+
+  console.log({ packageJsonStringNew, newPackageLockJson })
+
+  const packageJsonBase64New =
+    Buffer.from(packageJsonStringNew).toString('base64')
+  const packageLockJsonBase64New =
+    Buffer.from(newPackageLockJson).toString('base64')
+
+  const mainBranchRef = await octokit.rest.git.getRef({
+    owner,
+    repo,
+    ref: `heads/${baseBranch}`,
+  })
+
+  await octokit.rest.git.createRef({
+    owner,
+    repo,
+    ref: `refs/heads/${newBranch}`,
+    sha: mainBranchRef.data.object.sha,
+  })
+  console.log('created branch')
+
+  await context.octokit.repos.createOrUpdateFileContents({
+    owner,
+    repo,
+    path: packageJsonPath,
+    message: getCommitMessage(releasedRepo, tagName),
+    content: packageJsonBase64New,
+    branch: newBranch,
+    sha: packageJsonRef.data.sha,
+  })
+  await context.octokit.repos.createOrUpdateFileContents({
+    owner,
+    repo,
+    path: packageLockJsonPath,
+    message: getCommitMessage(releasedRepo, tagName),
+    content: packageLockJsonBase64New,
+    branch: newBranch,
+    sha: packageLockJsonRef.data.sha,
+  })
+  const pullRequestData = await octokit.rest.pulls.create({
+    owner,
+    repo,
+    head: newBranch,
+    base: baseBranch,
+    title: `feature: update ${releasedRepo} to version ${tagName}`,
+  })
+  await enableAutoSquash(octokit, pullRequestData)
+}
+
+/**
+ * @param {import('probot').Context<"release">} context
+ */
+const handleReleaseReleased = async (context) => {
+  await Promise.all([
+    updateBuiltinExtensions(context),
+    updateRendererProcessVersion(context),
+  ])
 }
 
 /**
---
+++ test/update-renderer-process.test.ts
@@ -0,0 +1,187 @@
+import * as nock from 'nock'
+import { Probot, ProbotOctokit } from 'probot'
+import * as myProbotApp from '../src/index.js'
+
+let probot: Probot | undefined
+
+beforeEach(() => {
+  nock.disableNetConnect()
+  probot = new Probot({
+    appId: 123,
+    privateKey: '123',
+    // disable request throttling and retries for testing
+    Octokit: ProbotOctokit.defaults({
+      retry: { enabled: false },
+      throttle: { enabled: false },
+    }),
+  })
+  probot.load(myProbotApp)
+})
+
+afterEach(() => {
+  nock.cleanAll()
+  nock.enableNetConnect()
+  probot = undefined
+})
+
+jest.mock('execa', () => {
+  return {
+    execa: jest.fn(),
+  }
+})
+
+jest.mock('node:fs/promises', () => {
+  return {
+    readFile: jest.fn(),
+    writeFile: jest.fn(),
+    mkdir: jest.fn(),
+  }
+})
+
+jest.mock('node:os', () => {
+  return {
+    tmpdir() {
+      return '/test'
+    },
+  }
+})
+
+const execa = require('execa')
+const fs = require('node:fs/promises')
+
+test('creates a pull request to update versions when a release is created', async () => {
+  let packageLockContent = ''
+  jest.spyOn(fs, 'readFile').mockImplementation((path) => {
+    if (typeof path === 'string' && path.endsWith('package-lock.json')) {
+      return packageLockContent
+    }
+    return ''
+  })
+  jest.spyOn(fs, 'writeFile').mockImplementation((path, content) => {
+    if (
+      typeof path === 'string' &&
+      path.endsWith('package-lock.json') &&
+      typeof content === 'string'
+    ) {
+      packageLockContent = content
+    }
+  })
+  jest.spyOn(execa, 'execa').mockImplementation(() => {
+    packageLockContent = JSON.stringify({
+      name: '@lvce-editor/renderer-worker',
+      version: '0.0.0-dev',
+      lockfileVersion: 3,
+      requires: true,
+      updated: true,
+    })
+  })
+  const mock = nock('https://api.github.com')
+    .get('/repos/lvce-editor/lvce-editor/git/ref/heads%2Fmain')
+    .reply(200, {
+      object: {
+        sha: 'main-sha',
+      },
+    })
+    .post('/repos/lvce-editor/lvce-editor/git/refs', (body) => {
+      expect(body).toEqual({
+        ref: 'refs/heads/update-version/renderer-process-v2.4.0',
+        sha: 'main-sha',
+      })
+      return true
+    })
+    .reply(200, {})
+    .get(
+      '/repos/lvce-editor/lvce-editor/contents/packages%2Frenderer-worker%2Fpackage.json',
+    )
+    .reply(200, {
+      content: Buffer.from(
+        JSON.stringify({
+          name: 'renderer-worker',
+          dependencies: {
+            '@lvce-editor/renderer-process': '^2.3.0',
+          },
+        }),
+      ),
+    })
+    .get(
+      '/repos/lvce-editor/lvce-editor/contents/packages%2Frenderer-worker%2Fpackage-lock.json',
+    )
+    .reply(200, {
+      content: Buffer.from(
+        JSON.stringify({
+          name: '@lvce-editor/renderer-worker',
+          version: '0.0.0-dev',
+          lockfileVersion: 3,
+          requires: true,
+        }),
+      ),
+    })
+    .put(
+      '/repos/lvce-editor/lvce-editor/contents/packages%2Frenderer-worker%2Fpackage.json',
+      (body) => {
+        expect(body).toEqual({
+          branch: 'update-version/renderer-process-v2.4.0',
+          content:
+            'ewogICJuYW1lIjogInJlbmRlcmVyLXdvcmtlciIsCiAgImRlcGVuZGVuY2llcyI6IHsKICAgICJAbHZjZS1lZGl0b3IvcmVuZGVyZXItcHJvY2VzcyI6ICJeMi40LjAiCiAgfQp9Cg==',
+          message: 'feature: update renderer-process to version v2.4.0',
+        })
+        return true
+      },
+    )
+    .reply(200)
+    .put(
+      '/repos/lvce-editor/lvce-editor/contents/packages%2Frenderer-worker%2Fpackage-lock.json',
+      (body) => {
+        expect(body).toEqual({
+          branch: 'update-version/renderer-process-v2.4.0',
+          content:
+            'eyJuYW1lIjoiQGx2Y2UtZWRpdG9yL3JlbmRlcmVyLXdvcmtlciIsInZlcnNpb24iOiIwLjAuMC1kZXYiLCJsb2NrZmlsZVZlcnNpb24iOjMsInJlcXVpcmVzIjp0cnVlLCJ1cGRhdGVkIjp0cnVlfQ==',
+          message: 'feature: update renderer-process to version v2.4.0',
+        })
+        return true
+      },
+    )
+    .reply(200)
+    .post(`/repos/lvce-editor/lvce-editor/pulls`, (body) => {
+      expect(body).toEqual({
+        base: 'main',
+        head: 'update-version/renderer-process-v2.4.0',
+        title: 'feature: update renderer-process to version v2.4.0',
+      })
+      return true
+    })
+    .reply(200, {
+      node_id: 'test-node-id',
+    })
+    .post('/graphql', (body) => {
+      expect(body).toEqual({
+        query: `mutation MyMutation {
+  enablePullRequestAutoMerge(input: { pullRequestId: \"test-node-id\", mergeMethod: SQUASH }) {
+    clientMutationId
+  }
+}
+`,
+      })
+      return true
+    })
+    .reply(200)
+
+  // Receive a webhook event
+  await probot.receive({
+    name: 'release',
+    payload: {
+      action: 'released',
+      release: {
+        tag_name: 'v2.4.0',
+      },
+      repository: {
+        name: 'renderer-process',
+        // @ts-ignore
+        owner: {
+          login: 'lvce-editor',
+        },
+      },
+    },
+  })
+  expect(mock.pendingMocks()).toEqual([])
+})
