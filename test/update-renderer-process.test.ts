74fc8960107f5a845c62eb69b46515427482ffc2
Le Vivilet <levivlet@pm.me>
2024-07-08 02:46:36 +0200

    test

--- test/update-renderer-process.test.ts
+++ test/update-renderer-process.test.ts
@@ -121,14 +121,14 @@ test('creates a pull request to update versions when a release is created', asyn
         tree: [
           {
             content:
-              'ewogICJuYW1lIjogInJlbmRlcmVyLXdvcmtlciIsCiAgImRlcGVuZGVuY2llcyI6IHsKICAgICJAbHZjZS1lZGl0b3IvcmVuZGVyZXItcHJvY2VzcyI6ICJeMi40LjAiCiAgfQp9Cg==',
+              '{"name":"@lvce-editor/renderer-worker","version":"0.0.0-dev","lockfileVersion":3,"requires":true,"updated":true}',
             mode: '100644',
             path: 'packages/renderer-worker/package.json',
             type: 'blob',
           },
           {
             content:
-              'eyJuYW1lIjoiQGx2Y2UtZWRpdG9yL3JlbmRlcmVyLXdvcmtlciIsInZlcnNpb24iOiIwLjAuMC1kZXYiLCJsb2NrZmlsZVZlcnNpb24iOjMsInJlcXVpcmVzIjp0cnVlLCJ1cGRhdGVkIjp0cnVlfQ==',
+              '{"name":"@lvce-editor/renderer-worker","version":"0.0.0-dev","lockfileVersion":3,"requires":true,"updated":true}',
             mode: '100644',
             path: 'packages/renderer-worker/package-lock.json',
             type: 'blob',
