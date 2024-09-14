29c1899b5fa5847021174f6d15ed626104430730
Le Vivilet <levivlet@pm.me>
2024-09-14 23:08:34 +0200

    feature: automatically update diff worker

--- src/index.js
+++ src/index.js
@@ -19,6 +19,11 @@ const dependencies = [
     toFolder: 'packages/renderer-worker',
   },
   {
+    fromRepo: 'diff-worker',
+    toRepo: 'lvce-editor',
+    toFolder: 'packages/renderer-worker',
+  },
+  {
     fromRepo: 'syntax-highlighting-worker',
     toRepo: 'lvce-editor',
     toFolder: 'packages/renderer-worker',
