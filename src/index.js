bcc8363a3586d98fc6acb2b4f2a0b4c019c85a07
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-05 10:57:11 +0200

    feature: automatically update iframe worker versions (#73)

--- src/index.js
+++ src/index.js
@@ -24,6 +24,11 @@ const dependencies = [
     toFolder: 'packages/renderer-worker',
   },
   {
+    fromRepo: 'iframe-worker',
+    toRepo: 'lvce-editor',
+    toFolder: 'packages/renderer-worker',
+  },
+  {
     fromRepo: 'extension-host-worker',
     toRepo: 'lvce-editor',
     toFolder: 'packages/renderer-worker',
