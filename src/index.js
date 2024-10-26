df039f72f4e6df0455491a5ff958b64001348fe6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-26 19:46:44 +0200

    feature: automatically update search view (#85)

--- src/index.js
+++ src/index.js
@@ -29,6 +29,11 @@ const dependencies = [
     toFolder: 'packages/renderer-worker',
   },
   {
+    fromRepo: 'extension-search-view',
+    toRepo: 'lvce-editor',
+    toFolder: 'packages/renderer-worker',
+  },
+  {
     fromRepo: 'iframe-worker',
     toRepo: 'lvce-editor',
     toFolder: 'packages/renderer-worker',
