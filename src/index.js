e516e2fd1aeb7148b7baac29b979829d5e54218d
Le Vivilet <levivlet@pm.me>
2024-12-10 20:25:42 +0100

    feature: automatically update title bar worker

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ const dependencies = [
     toFolder: 'packages/renderer-worker',
   },
   {
+    fromRepo: 'title-bar-worker',
+    toRepo: 'lvce-editor',
+    toFolder: 'packages/renderer-worker',
+  },
+  {
     fromRepo: 'explorer-view',
     toRepo: 'lvce-editor',
     toFolder: 'packages/renderer-worker',
