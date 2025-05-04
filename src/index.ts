96cccb909699694071ee8b8d9e88690d34a3c32c
Le Vivilet <levivlet@pm.me>
2025-05-04 13:43:29 +0200

    feature: automatically update rpc registry version in about view

--- src/index.ts
+++ src/index.ts
@@ -203,7 +203,12 @@ const dependencies = [
   {
     fromRepo: 'rpc',
     toRepo: 'about-view',
-    toFolder: '',
+    toFolder: 'packages/about-view',
+  },
+  {
+    fromRepo: 'rpc-registry',
+    toRepo: 'about-view',
+    toFolder: 'packages/about-view',
   },
   {
     fromRepo: 'rpc',
