ee244413efb1867af221cf64733ccb5a0060af90
Le Vivilet <levivlet@pm.me>
2025-06-13 23:04:47 +0200

    undo

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,7 +16,7 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x probot`
+  OUTPUT=`ncu -u -x probot jest @jest/globals`
   SUB='All dependencies match the latest package versions'
   if [[ "$OUTPUT" == *"$SUB"* ]]; then
     echo "$OUTPUT"
