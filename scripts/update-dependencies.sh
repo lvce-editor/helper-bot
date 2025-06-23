12dcc5d3d1efcde80ecdc68c85c2a3d7236ff812
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-23 14:42:29 +0200

    feature: update dependencies script (#260)

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,7 +16,7 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x probot jest @jest/globals`
+  OUTPUT=`ncu -u -x probot -x jest -x @jest/globals`
   SUB='All dependencies match the latest package versions'
   if [[ "$OUTPUT" == *"$SUB"* ]]; then
     echo "$OUTPUT"
