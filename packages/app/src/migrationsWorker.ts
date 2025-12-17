2058d69f612a6c3e7b595ab6fbfb235b3681dc57
Le Vivilet <levivilet@pm.me>
2025-12-17 20:25:51 +0100

    feature: use ts file in development (#407)

--- packages/app/src/migrationsWorker.ts
+++ packages/app/src/migrationsWorker.ts
@@ -1,15 +1,16 @@
 import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
 import { createLazyRpc } from '@lvce-editor/rpc-registry'
-import { migrationsWorkerUrl } from './migrationsWorkerUrl.ts'
+import { migrationsWorkerUrl, migrationsWorkerUrlDev } from './migrationsWorkerUrl.ts'
 
 const rpcId = 87
 
 export const { invoke, setFactory } = createLazyRpc(rpcId)
 
 const launchMigrationsWorker = async () => {
+  const workerUrl = process.env.NODE_ENV === 'production' ? migrationsWorkerUrl : migrationsWorkerUrlDev
   const rpc = await NodeWorkerRpcParent.create({
     commandMap: {},
-    path: migrationsWorkerUrl,
+    path: workerUrl,
   })
   return rpc
 }
