e95042c5bec92ff79ba809977271003f29753563
Le Vivilet <levivilet@pm.me>
2025-12-17 17:49:52 +0100

    fix: base endpoint

--- packages/migrations/src/parts/HandleHelloWorld/HandleHelloWorld.ts
+++ packages/migrations/src/parts/HandleHelloWorld/HandleHelloWorld.ts
@@ -1,8 +1,3 @@
-import { listCommands2 } from '../ListCommands2/ListCommands2.ts'
-
 export const handleHelloWorld = async () => {
-  const x = listCommands2()
-  return Response.json({
-    migrations: x,
-  })
+  return new Response('Hello World')
 }
