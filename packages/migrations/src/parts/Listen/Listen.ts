5b193c790dba512ea1ff1c25d07b192caf130d3c
Le Vivilet <levivilet@pm.me>
2025-12-17 14:36:54 +0100

    feature: filter out meta commands listCommands2 (#371)

--- packages/migrations/src/parts/Listen/Listen.ts
+++ packages/migrations/src/parts/Listen/Listen.ts
@@ -1,7 +1,9 @@
 import { NodeWorkerRpcClient } from '@lvce-editor/rpc'
 import { commandMap } from '../CommandMap/CommandMap.ts'
+import { commandMapRef } from '../CommandMapRef/CommandMapRef.ts'
 
 export const listen = async (): Promise<void> => {
+  Object.assign(commandMapRef, commandMap)
   await NodeWorkerRpcClient.create({
     commandMap,
   })
