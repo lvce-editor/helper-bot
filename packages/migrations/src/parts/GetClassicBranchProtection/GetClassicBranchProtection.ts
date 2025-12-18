0438c76dab6ff38ee51cc43a3ae5e958f28bde1a
Le Vivilet <levivilet@pm.me>
2025-12-18 01:42:31 +0100

    tsc

--- packages/migrations/src/parts/GetClassicBranchProtection/GetClassicBranchProtection.ts
+++ packages/migrations/src/parts/GetClassicBranchProtection/GetClassicBranchProtection.ts
@@ -3,33 +3,33 @@ import { VError } from '@lvce-editor/verror'
 
 export interface ClassicBranchProtection {
   readonly allow_deletions?: {
-    readonly enabled: boolean
+    readonly enabled?: boolean
   }
   readonly allow_force_pushes?: {
-    readonly enabled: boolean
+    readonly enabled?: boolean
   }
   readonly enforce_admins?: {
-    readonly enabled: boolean
+    readonly enabled?: boolean
   }
   readonly required_conversation_resolution?: {
-    readonly enabled: boolean
+    readonly enabled?: boolean
   }
   readonly required_linear_history?: {
-    readonly enabled: boolean
+    readonly enabled?: boolean
   }
   readonly required_pull_request_reviews?: {
-    readonly dismiss_stale_reviews: boolean
-    readonly require_code_owner_reviews: boolean
-    readonly required_approving_review_count: number
+    readonly dismiss_stale_reviews?: boolean
+    readonly require_code_owner_reviews?: boolean
+    readonly required_approving_review_count?: number
   }
   readonly required_status_checks?: {
-    readonly contexts: readonly string[]
-    readonly strict: boolean
+    readonly contexts?: readonly string[]
+    readonly strict?: boolean
   }
   readonly restrictions?: {
-    readonly apps: readonly string[]
-    readonly teams: readonly string[]
-    readonly users: readonly string[]
+    readonly apps?: readonly any[]
+    readonly teams?: readonly any[]
+    readonly users?: readonly any[]
   }
 }
 
