861ac0ec12d974043c54ae78ac708ef2e1d3a68e
Le Vivilet <levivilet@pm.me>
2025-12-27 16:16:09 +0100

    fix: exec worker url (#501)

--- packages/migrations/src/parts/ExecWorkerUrl/ExecWorkerUrl.ts
+++ packages/migrations/src/parts/ExecWorkerUrl/ExecWorkerUrl.ts
@@ -2,7 +2,7 @@ import { join } from 'path'
 
 const __dirname = import.meta.dirname
 
-const root = join(__dirname, '..', '..', '..')
+const root = join(__dirname, '..', '..', '..', '..', '..')
 
 export const execWorkerUrl = join(root, 'packages', 'exec-worker', 'dist', 'src', 'index.js')
 
