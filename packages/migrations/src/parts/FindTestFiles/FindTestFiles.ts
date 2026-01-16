d4051af874bfaa05f42acf149a0feea8769ab225
Le Vivilet <levivilet@pm.me>
2026-01-16 12:19:55 +0100

    fix

--- packages/migrations/src/parts/FindTestFiles/FindTestFiles.ts
+++ packages/migrations/src/parts/FindTestFiles/FindTestFiles.ts
@@ -3,13 +3,14 @@ import { listFiles } from '../ListFiles/ListFiles.ts'
 
 export const findTestFiles = async (clonedRepoUri: string, fs: Readonly<typeof FsPromises>): Promise<string[]> => {
   const allFiles = await listFiles(clonedRepoUri, fs)
-  const testFiles = allFiles.filter((file) => 
-    file.endsWith('.test.ts') ||
-    file.endsWith('.test.js') ||
-    file.endsWith('.spec.ts') ||
-    file.endsWith('.spec.js') ||
-    file.includes('.test.') ||
-    file.includes('.spec.')
+  const testFiles = allFiles.filter(
+    (file) =>
+      file.endsWith('.test.ts') ||
+      file.endsWith('.test.js') ||
+      file.endsWith('.spec.ts') ||
+      file.endsWith('.spec.js') ||
+      file.includes('.test.') ||
+      file.includes('.spec.'),
   )
   return testFiles
 }
