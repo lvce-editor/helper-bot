6ef9fae8b18a2976ca1cee23500343c6d98619a6
Le Vivilet <levivilet@pm.me>
2026-01-20 18:02:43 +0100

    regex

--- packages/migrations/src/parts/ReplaceMockRpcPattern/ReplaceMockRpcPattern.ts
+++ packages/migrations/src/parts/ReplaceMockRpcPattern/ReplaceMockRpcPattern.ts
@@ -1,9 +1,5 @@
+const RE_RPC = /const\s+rpc\s*=\s*(RendererWorker|EditorWorker|TextSearchWorker|FileSearchWorker|IframeWorker|VirtualDom)\.registerMockRpc/g
+
 export const replaceMockRpcPattern = (content: string): string => {
-  // Replace const rpc = Worker.registerMockRpc with using rpc = Worker.registerMockRpc
-  // This pattern matches: const + whitespace + rpc + whitespace + = + whitespace + WorkerName.registerMockRpc
-  // Supports: RendererWorker, EditorWorker, TextSearchWorker, FileSearchWorker, IframeWorker, VirtualDom
-  return content.replaceAll(
-    /const\s+rpc\s*=\s*(RendererWorker|EditorWorker|TextSearchWorker|FileSearchWorker|IframeWorker|VirtualDom)\.registerMockRpc/g,
-    'using rpc = $1.registerMockRpc'
-  )
+  return content.replaceAll(RE_RPC, 'using rpc = $1.registerMockRpc')
 }
