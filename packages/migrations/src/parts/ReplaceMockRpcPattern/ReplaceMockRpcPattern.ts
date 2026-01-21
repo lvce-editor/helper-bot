24572d23ec2cf41c773b56effb22b0dc802cd1f5
Le Vivilet <levivilet@pm.me>
2026-01-21 21:07:35 +0100

    feature: update mockrpc regex

--- packages/migrations/src/parts/ReplaceMockRpcPattern/ReplaceMockRpcPattern.ts
+++ packages/migrations/src/parts/ReplaceMockRpcPattern/ReplaceMockRpcPattern.ts
@@ -1,4 +1,4 @@
-const RE_RPC = /const\s+(\w+)\s*=\s*(RendererWorker|EditorWorker|TextSearchWorker|FileSearchWorker|IframeWorker|VirtualDom)\.registerMockRpc/g
+const RE_RPC = /const\s+(\w+)\s*=\s*(.*)\.registerMockRpc/g
 
 export const replaceMockRpcPattern = (content: string): string => {
   return content.replaceAll(RE_RPC, 'using $1 = $2.registerMockRpc')
