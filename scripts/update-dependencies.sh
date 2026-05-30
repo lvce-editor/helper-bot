0d0f1174d7588517d4667b84268bd9ef888ed556
Le Vivilet <levivilet@pm.me>
2026-05-30 09:14:19 +0200

    feature: update dependencies

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -29,6 +29,7 @@ function updateDependencies {
                                                        updateDependencies             &&
 cd packages/app                                   && updateDependencies && cd ../.. &&
 cd packages/build                                   && updateDependencies && cd ../.. &&
+cd packages/exec-worker                                     && updateDependencies && cd ../.. &&
 cd packages/migrations                                     && updateDependencies && cd ../.. &&
 cd packages/github-worker                                     && updateDependencies && cd ../.. &&
 
